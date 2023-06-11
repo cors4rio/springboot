aab1cce5eb5392b83d455aaffe4b8d2f70dcfcc6
Icaro Conceição <perauta09@outlook.com>
2023-06-11 20:33:10 -0300

    feat:

--- src/main/java/com/example/springboot/dtos/ProductRecordDto.java
+++ src/main/java/com/example/springboot/dtos/ProductRecordDto.java
@@ -1,2 +1,7 @@
-package com.example.springboot.dtos;public record ProductRecordDto() {
-}
+    package com.example.springboot.dtos;
+
+    import jakarta.validation.constraints.NotBlank;
+    import jakarta.validation.constraints.NotNull;
+
+    public record ProductRecordDto(@NotBlank String name, @NotNull String value) {
+    }
